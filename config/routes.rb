c5cf5ed8d083657a95dba6b8331a416020919ec8
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-23 10:30:06 -0700

    Add search route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :orders, only: [:index, :show]
   resources :users,  only: [:index, :show]
 
+  get '/search', to: 'search#index'
+
   namespace :api do
     namespace :v1 do
       resources :items, only: [:index, :show, :destroy, :create]
