d18ac880994805aa31f3aa7b659fbb0608706828
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-02-23 10:52:06 -0700

    Complete failing service spec

---
+++ spec/services/store_service_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+describe 'Store Service' do
+  context '.conn' do
+    it 'returns a Faraday object' do
+
+      expect(StoreService.conn).to be_a Faraday::Connection
+    end
+  end
+
+  context '.by_zip' do
+    before do
+      VCR.insert_cassette 'store_service_response', record: :new_episodes
+    end
+
+    after do
+      VCR.eject_cassette
+    end
+
+    it 'returns store count of 16 and 10 stores' do
+      expected_keys = [:longName, :storeType, :distance, :phone, :city]
+      zip = '80202'
+      stores_response = StoreService.by_zip(zip)
+
+      expect(stores_response).to be_a Hash
+      expect(stores_response).to have_key :store_count
+      expect(stores_response[:store_count]).to eq 16
+      expect(stores_response).to have_key :stores
+
+      raw_stores = stores_response[:stores]
+
+      expect(raw_stores).to be_a Array
+      expect(raw_stores.count).to eq 10
+
+      raw_store = raw_stores.first
+
+
+      expected_keys.each do |key|
+        expect(raw_store).to have_key key
+      end
+    end
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
